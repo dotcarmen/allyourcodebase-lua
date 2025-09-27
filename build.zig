999349519da2321b71ea60c6f0c2d441a19f05a5
Carmen <carmen@dotcarmen.dev>
2025-09-27 21:16:48 +0200

    more options

--- build.zig
+++ build.zig
@@ -9,20 +9,110 @@ const version = std.SemanticVersion{
     .patch = 8,
 };
 
+pub const Options = struct {
+    optimize: OptimizeMode,
+    target: ResolvedTarget,
+    shared: bool,
+    reentrant: bool,
+    compat_5_3: bool,
+    compat_mathlib: bool,
+    compat_apiintcasts: bool,
+    compat_lt_le: bool,
+    path_default: ?[]const []const u8,
+    cpath_default: ?[]const []const u8,
+    dirsep: ?[]const u8,
+    use_apicheck: bool,
+    use_c89: bool,
+    use_dlopen: bool,
+    use_ios: bool,
+    use_linux: bool,
+    use_macosx: bool,
+    use_posix: bool,
+    use_readline: bool,
+    luaconf_h: ?Build.LazyPath,
+};
+
+pub fn buildOptions(b: *Build, opts: struct {
+    optimize: OptimizeMode,
+    target: ResolvedTarget,
+}) Options {
+    const optimize = opts.optimize;
+    const target = opts.target.result;
+    return .{
+        .optimize = optimize,
+        .target = opts.target,
+
+        .shared = b.option(bool, "shared", "build as shared library") orelse target.isMinGW(),
+        .reentrant = b.option(bool, "reentrant", "enable reentrant lua") orelse (target.os.tag == .solaris),
+
+        .compat_5_3 = b.option(bool, "compat_5_3", "enable deprecated lua 5.3 apis") orelse false,
+        .compat_mathlib = b.option(bool, "compat_mathlib", "enable deprecated math apis") orelse false,
+        .compat_apiintcasts = b.option(bool, "compat_apiintcasts", "enable deprecated int manipulation macros") orelse false,
+        .compat_lt_le = b.option(bool, "compat_lt_le", "enable emulation of the __le metamethod using __lt") orelse false,
+
+        .path_default = b.option([]const []const u8, "path_default", "default PATH in Lua runtime"),
+        .cpath_default = b.option([]const []const u8, "cpath_default", "default CPATH in Lua runtime"),
+        .dirsep = b.option([]const u8, "dirsep", "directory separator for submodules"),
+
+        .use_apicheck = b.option(bool, "use_apicheck", "enable consistency checks in the C API") orelse
+            switch (optimize) {
+                .Debug, .ReleaseSafe => true,
+                .ReleaseFast, .ReleaseSmall => false,
+            },
+        .use_c89 = b.option(bool, "use_c89", "limit lua to c89 apis") orelse false,
+        .use_dlopen = b.option(bool, "use_dlopen", "enable dlopen support") orelse
+            switch (target.os.tag) {
+                .aix, .solaris => true,
+                else => false,
+            },
+        .use_ios = b.option(bool, "use_ios", "enable ios apis") orelse (target.os.tag == .ios),
+        .use_linux = b.option(bool, "use_linux", "enable linux apis") orelse
+            switch (target.os.tag) {
+                .linux, .freebsd, .netbsd, .openbsd => true,
+                else => false,
+            },
+        .use_macosx = b.option(bool, "use_macosx", "enable macos apis") orelse (target.os.tag == .macos),
+        .use_posix = b.option(bool, "use_posix", "enable posix apis") orelse
+            switch (target.os.tag) {
+                .aix, .solaris => true,
+                else => false,
+            },
+        .use_readline = b.option(bool, "use_readline", "enable readline apis") orelse
+            switch (target.os.tag) {
+                .freebsd, .netbsd, .openbsd, .macos => true,
+                else => false,
+            },
+
+        .luaconf_h = b.option(Build.LazyPath, "luaconf", "path to luaconf.h"),
+    };
+}
+
 pub fn build(b: *Build) !void {
     const target = b.standardTargetOptions(.{});
-    const optimize = b.standardOptimizeOption(.{ .preferred_optimize_mode = .ReleaseFast });
-
-    const build_shared = b.option(bool, "shared", "build as shared library") orelse target.result.isMinGW();
-    const use_readline =
-        if (target.result.os.tag == .linux)
-            b.option(bool, "use_readline", "readline support for linux") orelse false
-        else
-            null;
+    const optimize = b.standardOptimizeOption(.{});
+    const options = buildOptions(b, .{ .optimize = optimize, .target = target });
 
     const lua_src = b.dependency("lua", .{});
 
-    const lib_name = if (build_shared)
+    const headers = b.addNamedWriteFiles("headers");
+    const luaconf_h = options.luaconf_h orelse lua_src.path("src/luaconf.h");
+    _ = headers.addCopyFile(luaconf_h, "luaconf.h");
+    _ = headers.addCopyDirectory(lua_src.path("src"), "", .{
+        .include_extensions = &lua_inc,
+        .exclude_extensions = &.{"luaconf.h"},
+    });
+
+    var cflags: std.ArrayList([]const u8) = .empty;
+    try cflags.appendSlice(b.allocator, &base_cflags);
+    try cflags.appendSlice(b.allocator, &.{
+        if (options.use_c89) "-std=c89" else "-std=gnu99",
+        switch (optimize) {
+            .Debug, .ReleaseFast, .ReleaseSafe => "-O2",
+            .ReleaseSmall => "-Os",
+        },
+    });
+
+    const lib_name = if (options.shared)
         b.fmt("lua{d}{d}", .{ version.major, version.minor })
     else
         "lua";
@@ -33,19 +123,15 @@ pub fn build(b: *Build) !void {
             .link_libc = true,
             .optimize = optimize,
             .target = target,
-            .strip = if (build_shared and target.result.os.tag == .windows) true else null,
+            .strip = if (options.shared and target.result.os.tag == .windows) true else null,
         }),
     });
     lib.addCSourceFiles(.{
         .root = lua_src.path("src"),
         .files = &base_src,
-        .flags = &cflags,
+        .flags = cflags.items,
     });
-    lib.installHeadersDirectory(
-        lua_src.path("src"),
-        "",
-        .{ .include_extensions = &lua_inc },
-    );
+    lib.installHeadersDirectory(headers.getDirectory(), "", .{});
     b.installArtifact(lib);
 
     const exe = b.addExecutable(.{
@@ -58,7 +144,7 @@ pub fn build(b: *Build) !void {
     });
     exe.root_module.addCSourceFile(.{
         .file = lua_src.path("src/lua.c"),
-        .flags = &cflags,
+        .flags = cflags.items,
     });
     exe.root_module.linkLibrary(lib);
     b.installArtifact(exe);
@@ -73,7 +159,7 @@ pub fn build(b: *Build) !void {
     });
     exec.root_module.addCSourceFile(.{
         .file = lua_src.path("src/luac.c"),
-        .flags = &cflags,
+        .flags = cflags.items,
     });
     exec.root_module.linkLibrary(lib);
     b.installArtifact(exec);
@@ -87,53 +173,50 @@ pub fn build(b: *Build) !void {
     for (&build_targets) |obj| {
         obj.addIncludePath(lua_src.path("src"));
 
-        const link = obj != lib or !build_shared;
+        const link = obj != lib or !options.shared;
         if (link) {
             obj.linkSystemLibrary("m");
         }
 
+        const mod = obj.root_module;
+        if (options.reentrant) mod.addCMacro("_REENTRANT", "");
+        if (options.compat_5_3) mod.addCMacro("LUA_COMPAT_5_3", "");
+        if (options.compat_mathlib) mod.addCMacro("LUA_COMPAT_MATHLIB", "");
+        if (options.compat_apiintcasts) mod.addCMacro("LUA_COMPAT_APIINTCASTS", "");
+        if (options.compat_lt_le) mod.addCMacro("LUA_COMPAT_LT_LE", "");
+        if (options.dirsep) |lua_dirsep| mod.addCMacro("LUA_DIRSEP", lua_dirsep);
+        if (options.use_apicheck) mod.addCMacro("LUA_USE_APICHECK", "");
+        if (options.use_c89) mod.addCMacro("LUA_USEC89", "");
+        if (options.use_ios) mod.addCMacro("LUA_USE_IOS", "");
+        if (options.use_linux) mod.addCMacro("LUA_USE_LINUX", "");
+        if (options.use_macosx) mod.addCMacro("LUA_USE_MACOSX", "");
+        if (options.use_posix) mod.addCMacro("LUA_USE_POSIX", "");
+
+        if (options.path_default) |default_path| {
+            const path = try std.mem.join(b.allocator, ";", default_path);
+            mod.addCMacro("LUA_PATH_DEFAULT", path);
+        }
+
+        if (options.cpath_default) |default_cpath| {
+            const cpath = try std.mem.join(b.allocator, ";", default_cpath);
+            mod.addCMacro("LUA_CPATH_DEFAULT", cpath);
+        }
+
+        if (options.use_dlopen) {
+            mod.addCMacro("LUA_USE_DLOPEN", "");
+            if (link) obj.linkSystemLibrary("dl");
+        }
+
+        if (options.use_readline) {
+            mod.addCMacro("LUA_USE_READLINE", "");
+            if (link) obj.linkSystemLibrary("readline");
+        }
+
         switch (target.result.os.tag) {
-            .aix => {
-                obj.root_module.addCMacro("LUA_USE_POSIX", "");
-                obj.root_module.addCMacro("LUA_USE_DLOPEN", "");
-                if (link) {
-                    obj.linkSystemLibrary("dl");
-                }
-            },
             .freebsd, .netbsd, .openbsd => {
-                obj.root_module.addCMacro("LUA_USE_LINUX", "");
-                obj.root_module.addCMacro("LUA_USE_READLINE", "");
                 obj.addIncludePath(.{ .cwd_relative = "/usr/include/edit" });
                 if (link) {
                     obj.linkSystemLibrary("edit");
-                }
-            },
-            .ios => {
-                obj.root_module.addCMacro("LUA_USE_IOS", "");
-            },
-            .linux => {
-                obj.root_module.addCMacro("LUA_USE_LINUX", "");
-                obj.linkSystemLibrary("dl");
-                if (use_readline.?) {
-                    obj.root_module.addCMacro("LUA_USE_READLINE", "");
-                    if (link) {
-                        obj.linkSystemLibrary("readline");
-                    }
-                }
-            },
-            .macos => {
-                obj.root_module.addCMacro("LUA_USE_MACOSX", "");
-                obj.root_module.addCMacro("LUA_USE_READLINE", "");
-                if (link) {
-                    obj.linkSystemLibrary("readline");
-                }
-            },
-            .solaris => {
-                obj.root_module.addCMacro("LUA_USE_POSIX", "");
-                obj.root_module.addCMacro("LUA_USE_DLOPEN", "");
-                obj.root_module.addCMacro("_REENTRANT", "");
-                if (link) {
-                    obj.linkSystemLibrary("dl");
                 }
             },
             else => {},
@@ -164,8 +247,8 @@ pub fn build(b: *Build) !void {
     unpack_step.dependOn(&unpack_cmd.step);
 }
 
-const cflags = [_][]const u8{
-    "-std=gnu99",
+const base_cflags = [_][]const u8{
+    // "-std=gnu99",
     "-Wall",
     "-Wextra",
 };
